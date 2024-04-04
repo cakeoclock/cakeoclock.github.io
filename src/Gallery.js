75721b0086e91a108561f097d4c32a75d24a1134
Amit Kumar Shaw <amit.shaw@rwth-aachen.de>
2024-04-04 11:13:27 +0530

    tap gallery image

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,17 +1,30 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image, Modal } from "react-bootstrap";
 import images from "./galleryImages";
+import React, { useState } from "react";
 
 function Gallery() {
+
+  const [showDetails, setShowDetails] = useState(false);
+  const [productDetails, setProductDetails] = useState({});
+
+  const handleCloseDetails = () => setShowDetails(false);
+  const handleShowDetails = (productData) => {
+    setShowDetails(true);
+    setProductDetails(productData);
+  };
+
   return (
     <>
       <Container>
         <Row>
           {images.map((image, index) => (
             <Col xs={6} sm={6} md={4} lg={3} key={index} className="mb-4">
-              <img
+              <Image
                 src={`/gallery/${image}`}
                 alt={`Gallery ${index}`}
-                style={{ width: "100%", height: "200px", objectFit: "cover" }}
+                style={{ width: "100%", height: "200px", objectFit: "cover" }} 
+                rounded
+                onClick={() => handleShowDetails(image)}
               />
             </Col>
           ))}
@@ -22,6 +35,27 @@ function Gallery() {
           distance or free take away.
         </p> */}
       </Container>
+      <Modal
+        show={showDetails}
+        onHide={handleCloseDetails}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          {/* <Modal.Title id="contained-modal-title-vcenter">
+            {productDetails.name}
+          </Modal.Title> */}
+        </Modal.Header>
+        <Modal.Body>
+          <Image
+            variant="top"
+            src={`img/${productDetails}`}
+            alt={productDetails}
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+          
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
